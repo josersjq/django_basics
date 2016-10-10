a069a16d02712812be7587467f346f814b2e22f6
Jose <sanjuanjojo@gmail.com>
2016-10-10 19:19:18 +0800

    Deployment

--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 
 from .models import Note
 from .forms import NoteForm
+from django.http import Http404
 
 
 class Index(View):
@@ -23,4 +24,30 @@ class Index(View):
         context = self.get_context_data()
         if form.is_valid():
             form.save()
+        return render(request, self.template_name, context)
+
+
+class Update(View):
+    template_name = 'notes/update.html'
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        try:
+            note_id = kwargs.get('pk', 0)
+            note = Note.objects.get(id=note_id)
+        except Note.DoesNotExist:
+            raise Http404
+        else:
+            context['note'] = note
+            return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        form = NoteForm(request.POST, instance=context.get('note'))
+        if form.is_valid():
+            form.save
         return render(request, self.template_name, context)
